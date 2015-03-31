6bb302a2aec29bdfe4177f9bc64b17740de89d47
René Schuster <greenflash1357@gmx.net>
2015-03-31 11:03:40 +0200

    [IMP] horner renamed into undigit [ADD] digithist, isanagram

--- src/Digits.jl
+++ src/Digits.jl
@@ -1,5 +1,30 @@
 module Digits
 
-# package code goes here
+export
+  undigit
+  digithist
+  isanagram
+
+
+function undigit(l:Array{Int,1})
+  return foldr((a,b)->b*10+a,l)
+end
+
+function digithist(l::Array{Int,1})
+  return hist(l,-0.5:10)[2]
+end
+
+digithist(i::Int) = digithist(digits(i))
+
+function isanagram(a::Array{Int,1}, b::Array{Int,1})
+  return digithist(a) == digithist(b)
+end
+
+function isanagram(a::Int, b::Int)
+  return isanagram(digits(a),digits(b))
+end
+
+
 
 end # module
+
