46281f48f9409bea1dc4b3a780c775b67aeab87e
René Schuster <greenflash1357@gmx.net>
2015-03-31 18:12:27 +0200

    [ADD] require [ADD] new functions [ADD] tests

--- src/Digits.jl
+++ src/Digits.jl
@@ -1,12 +1,17 @@
 module Digits
 
 export
-  undigit
-  digithist
-  isanagram
+  reversedigits,
+  undigit,
+  digithist,
+  isanagram,
+  ispalindrom,
+  contains
 
 
-function undigit(l:Array{Int,1})
+reversedigits(n::Int) = reverse!(digits(n))
+
+function undigit(l::Array{Int,1})
   return foldr((a,b)->b*10+a,l)
 end
 
@@ -14,7 +19,7 @@ function digithist(l::Array{Int,1})
   return hist(l,-0.5:10)[2]
 end
 
-digithist(i::Int) = digithist(digits(i))
+digithist(n::Int) = digithist(digits(n))
 
 function isanagram(a::Array{Int,1}, b::Array{Int,1})
   return digithist(a) == digithist(b)
@@ -24,7 +29,49 @@ function isanagram(a::Int, b::Int)
   return isanagram(digits(a),digits(b))
 end
 
+function ispalindrom(l::Array{Int,1})
+  return l == reverse(l)
+  #TODO: possible performance improvement: loop only over half the list
+end
+
+function ispalindrom2(l::Array{Int,1})
+  m = div(length(l),2)
+  for i = 1:m
+    if l[i] != l[end-i+1]
+      return false
+    end
+  end
+  return true
+end
+
+ispalindrom(n::Int) = ispalindrom(digits(n))
+
+function contains(a::Array{Int,1},b::Array{Int,1})
+  la = length(a)
+  lb = length(b)
+  if la > lb
+    for i = 1:la-lb+1
+      if a[i:i+lb-1] == b
+        return true
+      end
+    end
+  elseif lb > la
+    for i = 1:lb-la+1
+      if b[i:i+la-1] == a
+        return true
+      end
+    end
+  elseif a == b
+    return true
+  end
+  return false
+end
+
+contains(a::Array{Int,1},b::Int) = contains(a,digits(b))
+contains(a::Int,b::Array{Int,1}) = contains(digits(a),b)
+contains(a::Int,b::Int) = contains(digits(a),digits(b))
+
+
 
 
 end # module
-
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,34 @@
 using Digits
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+
+# test data
+n = 1234567890
+p = 1234567890987654321
+l = digits(n)
+lp = digits(p)
+
+# undigit
+@test undigit(l) == n
+
+# digithist
+@test digithist(l) == [1,1,1,1,1,1,1,1,1,1]
+@test digithist(n) == [1,1,1,1,1,1,1,1,1,1]
+@test digithist(p) == [1,2,2,2,2,2,2,2,2,2]
+
+# isanagram
+@test isanagram(122334,431232) == true
+@test isanagram(digits(122334),digits(431232)) == true
+@test isanagram(122334,433232) == false
+
+# ispalindrom
+@test ispalindrom(p) == true
+@test ispalindrom(lp) == true
+@test ispalindrom(n) == false
+
+# contains
+@test contains(n,3456) == true
+@test contains(l,567) == true
+@test contains(n,digits(123)) == true
+@test contains(l,digits(789)) == true
+@test contains(n,457) == false
