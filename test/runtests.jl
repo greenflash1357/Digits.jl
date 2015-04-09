cb3e194fca6050b5e634585dcbb62e54c1eef252
René Schuster <greenflash1357@gmx.net>
2015-04-09 12:48:07 +0200

    [IMP] more covering test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using Digits
 using Base.Test
 
-#TODO: add error test cases
 
 # test data
 n = 1234567890
@@ -27,6 +26,7 @@ lp = digits(p)
 @test isanagram(122334,431232) == true
 @test isanagram(digits(122334),digits(431232)) == true
 @test isanagram(122334,433232) == false
+@test isanagram(135,61436) == false
 
 # ispalindrom
 @test ispalindrome(p) == true
@@ -34,21 +34,27 @@ lp = digits(p)
 @test ispalindrome(n) == false
 
 # contains
+@test contains(n,n) == true
 @test contains(n,3456) == true
+@test contains(56,n) == true
 @test contains(l,567) == true
 @test contains(n,digits(123)) == true
 @test contains(l,digits(789)) == true
 @test contains(n,457) == false
 
 # startswith
+@test startswith(n,n) == true
 @test startswith(n,123) == true
+@test startswith(12,n) == true
 @test startswith(l,1234) == true
 @test startswith(l,digits(12)) == true
 @test startswith(n,digits(1)) == true
 @test startswith(n,2345) == false
 
 # endswith
+@test endswith(n,n) == true
 @test endswith(n,7890) == true
+@test endswith(90,n) == true
 @test endswith(l,90) == true
 @test endswith(l,digits(90)) == true
 @test endswith(n,digits(567890)) == true
@@ -63,6 +69,7 @@ lp = digits(p)
 @test crop(l,10) == [0]
 @test crop(n,0) == n
 @test crop(l,0) == l
+@test_throws ErrorException crop(n,11)
 
 # combine
 @test combine(0,23) == 23
@@ -70,8 +77,7 @@ lp = digits(p)
 @test combine(976,23) == 97623
 @test combine(digits(234),digits(46)) == digits(23446)
 @test combine(l,[0]) == digits(12345678900)
-# how to handle leading zeros?
-# @test combine([0],l) == [0,l]
+@test combine([0],l) == [l,0]
 @test combine(Int[],l) == l
 @test combine(l,Int[]) == l
 
